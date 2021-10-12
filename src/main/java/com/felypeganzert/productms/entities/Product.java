87d1dbade79a53a383e37957320aefce80d1ba21
Felype Ganzert <felipe.ganzert@gmail.com>
2021-10-12 11:58:13 -0300

    Create Product.java

---
+++ src/main/java/com/felypeganzert/productms/entities/Product.java
@@ -0,0 +1,44 @@
+package com.felypeganzert.productms.entities;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotBlank(message = "Name é obrigatório")
+    private String name;
+
+    @NotBlank(message = "Description é obrigatório")
+    private String description;
+
+    @NotNull
+    @Positive( message = "Preço deve ser positivo")
+    private BigDecimal price;
+    
+}
