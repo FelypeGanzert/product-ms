4f97465fc12e66700c264fa43f28a645d2f5f555
Felype Ganzert <felipe.ganzert@gmail.com>
2021-10-12 14:49:59 -0300

    Create RecursoNaoEncontradoException.java

---
+++ src/main/java/com/felypeganzert/productms/exceptions/RecursoNaoEncontradoException.java
@@ -0,0 +1,22 @@
+package com.felypeganzert.productms.exceptions;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+    
+    private static final long serialVersionUID = 1L;
+
+	private String nomeRecurso;
+	private String nomeCampo;
+	private Object valorCampo;
+
+	public RecursoNaoEncontradoException(String nomeRecurso, String nomeCampo, Object valorCampo) {
+		super();
+		this.nomeRecurso = nomeRecurso;
+		this.nomeCampo = nomeCampo;
+		this.valorCampo = valorCampo;
+	}
+
+	public String getMessage(){
+		return String.format("%s não encontrado com %s: '%s'", nomeRecurso, nomeCampo, valorCampo);
+	}
+
+}
