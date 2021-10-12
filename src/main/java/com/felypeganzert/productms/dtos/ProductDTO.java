18166b2c99ee162f2710edce84eac73df0585d54
Felype Ganzert <felipe.ganzert@gmail.com>
2021-10-12 14:07:28 -0300

    Create ProductDTO.java

---
+++ src/main/java/com/felypeganzert/productms/dtos/ProductDTO.java
@@ -0,0 +1,31 @@
+package com.felypeganzert.productms.dtos;
+
+import java.math.BigDecimal;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class ProductDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Name é obrigatório")
+    private String name;
+
+    @NotBlank(message = "Description é obrigatório")
+    private String description;
+
+    @NotNull
+    @Positive( message = "Preço deve ser positivo")
+    private BigDecimal price;
+}
