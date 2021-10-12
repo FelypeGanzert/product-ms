e01d80d65a5eb0f842893252bfd0d4e4c390b694
Felype Ganzert <felipe.ganzert@gmail.com>
2021-10-12 14:50:53 -0300

    Create AppConstantes.java

---
+++ src/main/java/com/felypeganzert/productms/utils/AppConstantes.java
@@ -0,0 +1,9 @@
+package com.felypeganzert.productms.utils;
+
+public class AppConstantes {
+
+    public static final String ID = "id";
+
+    public static final String PRODUCT = "Product";
+    
+}
